e229a41dd20382ec271c2b7bfe0c635899e8c964
Qume1 <semyon.sirotin@2013@yandex.ru>
2022-05-06 21:20:38 +0300

    init

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,3 +1,13 @@
+import json
+txt_file = "output.txt"
+json_file = "output.json"
 if __name__ == "__main__":
-    # Write your solution here
+    list_ = [i ** 2 for i in range(1, 11)]
+    with open(txt_file, "w") as f:
+        f.write(str(list_))
+
+    with open(json_file, "w") as f:
+        with open(txt_file, "r") as d:
+            json.dump(json.load(d), f, indent=4)
+
     pass
