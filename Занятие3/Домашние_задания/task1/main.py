7599db50cae12def4cbc7e2dbb0d393b8efc6dba
Qume1 <semyon.sirotin@2013@yandex.ru>
2022-05-13 23:58:18 +0300

    init

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,6 +1,8 @@
 import json
+import pickle
 txt_file = "output.txt"
 json_file = "output.json"
+pickle_file = "output.pickle"
 if __name__ == "__main__":
     list_ = [i ** 2 for i in range(1, 11)]
     with open(txt_file, "w") as f:
@@ -9,5 +11,12 @@ if __name__ == "__main__":
     with open(json_file, "w") as f:
         with open(txt_file, "r") as d:
             json.dump(json.load(d), f, indent=4)
+
+    with open(pickle_file, "wb") as f:
+        with open(txt_file, "r") as d:
+            pickle.dump(pickle.load(d), f)
+
+    with open(pickle_file, "rb") as f:
+        obj = pickle.load(f)
 
     pass
